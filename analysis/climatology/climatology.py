0a63f81b2a88b6f199f82a7836aa85513a03ed0b
Christian Schroeder <caschroeder@outlook.com>
2020-12-15 22:47:47 +0100

    updated climatology

--- analysis/climatology/climatology.py
+++ analysis/climatology/climatology.py
@@ -9,17 +9,17 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.dataloader.memmap_dataloader import Dataset
 from src.benchmark.utils import compute_latitude_weighting, compute_weighted_mse
 
-
 if __name__ == "__main__":
 
-    memmap_root = "TODO"  # SET MEMMAP DATA ROOT PATH HERE
-    memmap_root2 = "TODO"
+    memmap_root = "SET_THIS"  # SET MEMMAP DATA ROOT PATH HERE
+    memmap_root2 = "SET_THIS"
     datapath = [os.path.join(memmap_root, "imerg_5625", "imerg_5625.dill"),
                 os.path.join(memmap_root2, "era5625_mf", "era5625_mf.dill"),
                 ]
 
-    daterange_imerg = (datetime(2016, 4, 1), datetime(2017, 12, 31, 21))
-    daterange_era = daterange_imerg
+    daterange_imerg = (datetime(2000, 6, 1,0), datetime(2017, 12, 31, 23))
+    daterange_era = (datetime(1979, 1, 1, 7), datetime(2017, 12, 31, 23))
+
     daterange_val = (datetime(2018,1,6,0), datetime(2018, 12,31,23))
 
     partition_conf = {"era":
@@ -34,8 +34,8 @@ if __name__ == "__main__":
                       }
 
     partition_type = "range"
-    sample_conf_era = {"m0": {"era":{"tp_era": {"vbl": "era5625/tp"},"lat2d": {"vbl":"era5625/lat2d"}}}}
-    sample_conf_imerg = {"m0": {"era":{"imerg": {"vbl": "imerg5625/precipitationcal"}}}}
+    sample_conf_era = {"m0": {"era":{"tp_era": {"vbl": "era5625/tp"},"lat2d": {"vbl":"era5625/lat2d"}}}} # sample modes
+    sample_conf_imerg = {"m0": {"era":{"imerg": {"vbl": "imerg5625/precipitationcal"}}}} # sample modes
 
     dataset_era = Dataset(datapath=datapath,
                           partition_conf=partition_conf,
@@ -49,6 +49,7 @@ if __name__ == "__main__":
 
     era_dict = {}
     era_dict_ctr = {}
+
     # calculate weekly climatology for ERA5
     for i, d in enumerate(dataset_era[(daterange_era[0].timestamp(), daterange_era[1].timestamp(), 3600),["era5625/tp"], {}]):
         t = daterange_era[0] + timedelta(seconds=i*3600)
@@ -63,6 +64,8 @@ if __name__ == "__main__":
             era_dict[week] = np.array(d)
         pass
     print(sorted(era_dict.keys()))
+
+    era_annual_climatology = dataset_era[(daterange_era[0].timestamp(), daterange_era[1].timestamp(), 3600),["era5625/tp"], {}].mean(axis=0)
     del dataset_era
 
     with open("era_climatology.pickle", "wb") as f:
@@ -91,8 +94,9 @@ if __name__ == "__main__":
             imerg_dict[week] = np.array(d)
         pass
     print(sorted(imerg_dict.keys()))
-    del dataset_imerg
 
+    imerg_annual_climatology = dataset_imerg[ (daterange_imerg[0].timestamp(), daterange_imerg[1].timestamp(), 3600) , ["imerg5625/precipitationcal"], {}].mean(axis=0)
+    del dataset_imerg
     with open("imerg_climatology.pickle", "wb") as f:
         pickle.dump(era_dict, f)
 
@@ -111,9 +115,18 @@ if __name__ == "__main__":
         rms_error = rms_error**0.5
         re += (rms_error - re) / float(i+1)
         pass
+
+    print("ERA WEEKLY RMS:", re)
+
+    re = 0
+    for i, d in enumerate(dataset_era[(daterange_val[0].timestamp(), daterange_val[1].timestamp(), 3600), ["era5625/tp"], {}]):
+        rms_error = compute_weighted_mse(th.from_numpy(d)*1000, th.from_numpy(era_annual_climatology)*1000, th.from_numpy(lat_grid))
+        rms_error = rms_error**0.5
+        re += (rms_error - re) / float(i+1)
+        pass
     del dataset_era
 
-    print("ERA RMS:", re)
+    print("ERA ANNUAL RMS:", re)
 
     ########################## Predict IMERG
     dataset_imerg = Dataset(datapath=datapath,
@@ -124,13 +137,24 @@ if __name__ == "__main__":
                           )
 
     re = 0
-    for i, d in enumerate(dataset_imerg[(daterange_val[0].timestamp(), daterange_val[1].timestamp(), 1800), ["imerg5625/precipitationcal"], {}]):
-        t = daterange_val[0] + timedelta(seconds=i * 1800)
+    for i, d in enumerate(dataset_imerg[(daterange_val[0].timestamp(), daterange_val[1].timestamp(), 3600), ["imerg5625/precipitationcal"], {}]):
+        t = daterange_val[0] + timedelta(seconds=i * 3600)
         week = t.isocalendar()[1]
         rms_error = compute_weighted_mse(th.from_numpy(d), th.from_numpy(imerg_dict[week]), th.from_numpy(lat_grid))
         rms_error = rms_error**0.5
         re += (rms_error - re) / float(i+1)
         pass
+
+    print("IMERG WEEKLY RMS:", re)
+
+    re = 0
+    for i, d in enumerate(dataset_imerg[(daterange_val[0].timestamp(), daterange_val[1].timestamp(), 3600), ["imerg5625/precipitationcal"], {}]):
+        rms_error = compute_weighted_mse(th.from_numpy(d), th.from_numpy(imerg_annual_climatology), th.from_numpy(lat_grid))
+        rms_error = rms_error**0.5
+        re += (rms_error - re) / float(i+1)
+        pass
     del dataset_imerg
 
-    print("IMERG RMS:", re)
+    print("IMERG ANNUAL RMS:", re)
+
+
